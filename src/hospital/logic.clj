dcf28b2eaf5192f0c77c728f68f7cbe01c3eb149
esterlilianlb <esterlilian2231@gmail.com>
2020-08-10 19:01:21 -0300

    entendendo os problemas de redefinir simbolos globais quando temos threads concorrentes

--- src/hospital/logic.clj
+++ src/hospital/logic.clj
@@ -12,6 +12,13 @@
       (update hospital departamento conj pessoa)
       (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
 
+(defn chega-em-pausado
+  [hospital departamento pessoa]
+  (if (cabe-na-fila? hospital departamento)
+    (do (Thread/sleep 1000)
+    (update hospital departamento conj pessoa))
+    (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
+
 
 (defn atende [hospital departamento]
   (update hospital departamento pop))
