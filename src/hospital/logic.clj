987eaf5373299d653957af834233e2dd8c1dd90a
esterlilianlb <esterlilian2231@gmail.com>
2020-08-11 16:59:00 -0300

    entendendo o funcionamento do swap

--- src/hospital/logic.clj
+++ src/hospital/logic.clj
@@ -12,10 +12,15 @@
       (update hospital departamento conj pessoa)
       (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
 
-(defn chega-em-pausado
+;função malvada que parece ser pura mas usa random e altera estado do random e loga
+(defn chega-em-pausado-logando
   [hospital departamento pessoa]
+  (println "tentando adicionar a pessoa" pessoa)
+  (Thread/sleep (* (rand) 2000))
   (if (cabe-na-fila? hospital departamento)
-    (do (Thread/sleep 1000)
+    (do
+      ;(Thread/sleep 1000)
+    (println "dando update" pessoa)
     (update hospital departamento conj pessoa))
     (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
 
